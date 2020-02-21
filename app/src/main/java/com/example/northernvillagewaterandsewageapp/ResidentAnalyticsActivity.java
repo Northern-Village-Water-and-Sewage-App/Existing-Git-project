6f612f1acb36762e5be970f92df904c299ba5016
Zebedee <zebedee.weetaluktuk@gmail.com>
2020-02-20 22:54:25 -0500

    Added activities

---
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/ResidentAnalyticsActivity.java
@@ -0,0 +1,14 @@
+package com.example.northernvillagewaterandsewageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResidentAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resident_analytics);
+    }
+}
