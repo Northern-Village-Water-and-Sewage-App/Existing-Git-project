ea30991ed27ed4bfcd30566f498cf9dae433d6c6
Zebedee <zebedee.weetaluktuk@gmail.com>
2020-02-22 00:22:28 -0500

    Added "Add User Fragment"

---
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/CreateUserFragment.java
@@ -0,0 +1,74 @@
+package com.example.northernvillagewaterandsewageapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class CreateUserFragment extends DialogFragment {
+
+    protected EditText UsernameEditText;
+    protected EditText HouseNumberEditText;
+    protected EditText PinEditText;
+    protected Button RandomPinButton;
+    protected Button AddUserButton;
+    protected Button CancelAddUserButton;
+    protected Button RemoveUserButton;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_create_user, container, false);
+
+        //connects the edit texts to the Java file
+        UsernameEditText = view.findViewById(R.id.userNameEditText);
+        HouseNumberEditText = view.findViewById(R.id.housNumEditText);
+        PinEditText = view.findViewById(R.id.pinEditText);
+        //connects the buttons to the Java file
+        RandomPinButton = view.findViewById(R.id.pinButton);
+        AddUserButton = view.findViewById(R.id.addUserButton);
+        CancelAddUserButton = view.findViewById(R.id.cancelAddUserButton);
+        RemoveUserButton = view.findViewById(R.id.removeUserButton);
+
+        //Makes buttons do something on click
+        RandomPinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                                                                                                                                                    //******
+            }
+        });
+        AddUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {                                                                                                                           //******
+
+            }
+        });
+        CancelAddUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+        RemoveUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                                                                                                                                                    //*******
+            }
+        });
+
+
+
+
+
+       return view;
+    }
+}
