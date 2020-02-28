7c6e870d4ef14fb56d4e127ff6f083daa3e635bd
Zebedee <zebedee.weetaluktuk@gmail.com>
2020-02-27 20:47:14 -0500

    Layout of disable user fragment

--- app/src/main/java/com/example/northernvillagewaterandsewageapp/DisableUserFragment.java
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/DisableUserFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,13 +16,10 @@ import androidx.fragment.app.DialogFragment;
 public class DisableUserFragment extends DialogFragment {
 
     protected EditText UsernameEditText;
-    protected EditText PinEditText;
-    protected Button RandomPinButton;
-    protected Button AddUserButton;
-    protected Button CancelAddUserButton;
-    protected Button RemoveUserButton;
-
-
+    protected EditText timeDisableUserEditText;
+    protected Button CancelDisableUserButton;
+    protected Button ConfirmDisableUserButton;
+    protected Integer TimeTypeDisableUser;
 
     @Nullable
     @Override
@@ -28,7 +27,33 @@ public class DisableUserFragment extends DialogFragment {
 
         View view = inflater.inflate(R.layout.fragment_disable_user, container, false);
 
+        //attaches the XML file to the java
+        UsernameEditText = view.findViewById(R.id.UserIDDisableUserEditText);
+        timeDisableUserEditText = view.findViewById(R.id.timeDIsableUserEditText);
+        CancelDisableUserButton = view.findViewById(R.id.cancelDisableUserButton);
+        ConfirmDisableUserButton = view.findViewById(R.id.confirmDisableUserButton);
 
+        //get steh dropdown menu to work
+        Spinner TimeTypeSpinner = view.findViewById(R.id.timesDisableUserSpinner);
+        TimeTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TimeTypeDisableUser = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //gets the dismiss button to get rid of the fragment and go back to whatever called this.
+        CancelDisableUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
 
        return view;
     }
