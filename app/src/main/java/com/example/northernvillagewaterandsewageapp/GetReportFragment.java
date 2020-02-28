b5fe50f593cbc72c1510e9a0afc8f18d24b6dfad
Zebedee <zebedee.weetaluktuk@gmail.com>
2020-02-27 21:59:24 -0500

    Layout of get report fragment

---
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/GetReportFragment.java
@@ -0,0 +1,76 @@
+package com.example.northernvillagewaterandsewageapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class GetReportFragment extends DialogFragment {
+
+    protected EditText EmailAdressEditText;
+    protected Integer Company;
+    protected Button GetAllReportsButton;
+    protected Button GetCompanyReportsButton;
+    protected Button CancelGetReportButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_get_report, container, false);
+
+        //connects the XML file to the Java
+        EmailAdressEditText = view.findViewById(R.id.emailAddressReportEditText);
+        GetAllReportsButton = view.findViewById(R.id.allReportsButton);
+        GetCompanyReportsButton = view.findViewById(R.id.ReportsByCompanyButton);
+        CancelGetReportButton = view.findViewById(R.id.cancelGetReportsButton);
+
+        //gets the drop down menu to work
+        Spinner companyReportsSpinner = view.findViewById(R.id.companyReportSpinner);
+        companyReportsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Company = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //gets the cancel button to dismiss the fragment when pressed
+        CancelGetReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        //what happens when this button is pressed?
+        GetAllReportsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //what happens when this button is pressed?
+        GetCompanyReportsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+       return view;
+    }
+}
