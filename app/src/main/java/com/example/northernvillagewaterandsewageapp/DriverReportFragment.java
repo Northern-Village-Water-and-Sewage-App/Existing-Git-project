90956e337c5f0c8d7214579727606b7e7d756354
Zebedee <zebedee.weetaluktuk@gmail.com>
2020-02-27 21:26:38 -0500

    Layout of driver report fragment

--- app/src/main/java/com/example/northernvillagewaterandsewageapp/DriverReportFragment.java
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/DriverReportFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +15,10 @@ import androidx.fragment.app.DialogFragment;
 
 public class DriverReportFragment extends DialogFragment {
 
-    protected EditText UsernameEditText;
-    protected EditText PinEditText;
-    protected Button RandomPinButton;
-    protected Button AddUserButton;
-    protected Button CancelAddUserButton;
-    protected Button RemoveUserButton;
-
-
+    protected Integer ComplaintType;
+    protected EditText Complaint;
+    protected Button CancelDriverReport;
+    protected Button ConfirmDriverReport;
 
     @Nullable
     @Override
@@ -28,7 +26,40 @@ public class DriverReportFragment extends DialogFragment {
 
         View view = inflater.inflate(R.layout.fragment_driver_report, container, false);
 
+        //connects the XML file to the Java
+        Complaint = view.findViewById(R.id.complaintDriverReportEditText);
+        CancelDriverReport = view.findViewById(R.id.cancelDriverReportButton);
+        ConfirmDriverReport = view.findViewById(R.id.confirmDriverReportButton);
 
+        //gets the drop down menu to work
+        Spinner complaintTypeSpinner = view.findViewById(R.id.complaintTypeDriverReportSpinner);
+        complaintTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ComplaintType = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //gets the cancel button to dismiss the fragment when pressed
+        CancelDriverReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        //what happens when this button is pressed?
+        ConfirmDriverReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
        return view;
     }
