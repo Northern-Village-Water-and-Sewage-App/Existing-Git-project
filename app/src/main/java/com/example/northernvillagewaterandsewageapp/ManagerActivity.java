ca091c9b4b8bafa870e69e0e11d782dc449bf44c
Ali Abbas <v2thegreat@gmail.com>
2020-03-23 16:04:33 -0400

    Added functionality for manager work list

--- app/src/main/java/com/example/northernvillagewaterandsewageapp/ManagerActivity.java
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/ManagerActivity.java
@@ -16,7 +16,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.northernvillagewaterandsewageapp.Fragments.CreateDriverFragment;
 import com.example.northernvillagewaterandsewageapp.Fragments.CreateResidentFragment;
 import com.example.northernvillagewaterandsewageapp.Fragments.DisableUserFragment;
@@ -24,6 +31,10 @@ import com.example.northernvillagewaterandsewageapp.Fragments.GetReportFragment;
 import com.example.northernvillagewaterandsewageapp.Fragments.ManagerTimeEstimateFragment;
 import com.example.northernvillagewaterandsewageapp.Fragments.ManualDemandFragment;
 import com.example.northernvillagewaterandsewageapp.Fragments.MessageFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -34,11 +45,13 @@ public class ManagerActivity extends AppCompatActivity {
     protected ListView worklistListView;
     protected Integer HiddenOption;
 
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+        mQueue = Volley.newRequestQueue(this);
 
         setUpManagerUI();
 
@@ -111,33 +124,50 @@ public class ManagerActivity extends AppCompatActivity {
     }
 
 
-
-    //LoadListViewFunction
-    protected void loadListView(){
-        ArrayList<String> worklistListText = new ArrayList<>();
-
-        //makes a list item                                                               //Fix here to make the list view correct
-        for(int i = 0; i < 5; i++)
-        {
-            String temp = "";
-            temp+= "House Number: " + i * 49 + 8 + "\n";
-            temp+= "Service: " + "Water" + "\n";
-            temp+= "Time Estimate: " + "None" + "\n";
-
-            worklistListText.add(temp);
-        }
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, worklistListText);
-        worklistListView.setAdapter(arrayAdapter);
-
-        //makes clicking on an item from the worklist pull up the manager time estimate fragment, with the information it needs to update the database
-        worklistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void loadListView() {
+        String url = "http://13.59.214.194:5000/get_work_list/";
+        final ArrayList<String> worklistListText = new ArrayList<>();
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        final JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               //need to pass other stuff here to make this work                                                                                                            ************************
-                ManagerTimeEstimateFragment managerTimeEstimateFragment = new ManagerTimeEstimateFragment();
-               managerTimeEstimateFragment.show(getSupportFragmentManager(), "Dialog");
+            public void onResponse(JSONArray response) {
+                try {
+                    JSONObject e;
+                    for (int pos = 0; pos < response.length(); pos ++ ){
+                        String temp = "";
+                        e = response.getJSONObject(pos);
+                        temp += "Time Stamp: " + e.getString("timestamp") + "\n";
+                        temp += "Resident: " + e.getString("username") + "\n";
+                        temp += "House Number: " + e.getString("house_number") + "\n";
+                        temp += "Tank Type: " + e.getString("tank_type") + "\n";
+                        temp += "Time Estimate: " + e.getString("estimate") + "\n";
+                        worklistListText.add(temp);
+
+                    }
+                    arrayAdapter.addAll(worklistListText);
+                    worklistListView.setAdapter(arrayAdapter);
+
+                    //makes clicking on an item from the worklist pull up the manager time estimate fragment, with the information it needs to update the database
+                    worklistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            //need to pass other stuff here to make this work                                                                                                            ************************
+                            ManagerTimeEstimateFragment managerTimeEstimateFragment = new ManagerTimeEstimateFragment();
+                            managerTimeEstimateFragment.show(getSupportFragmentManager(), "Dialog");
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        mQueue.add(request);
     }
 
     // Function that shows the manager analytics when its button is clicked
