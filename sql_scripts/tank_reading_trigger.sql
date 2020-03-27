5e82c2c5ac8d65da7eceec5a611eb87d169c4d7e
Ali Abbas Zaidi <v2thegreat@gmail.com>
2020-03-27 17:54:41 -0400

    Updated it to keep it up with newer models

--- sql_scripts/tank_reading_trigger.sql
+++ sql_scripts/tank_reading_trigger.sql
@@ -1,9 +1,9 @@
 create function is_caution_level(reading_pk int) returns table(is_caution bool) as
 $$
 begin
-    return QUERY select tr.current_height <= tm.tank_caution_level
-                 from tank_readings as tr
-                          join tank_model tm on tr.tank_model_fk = tm.pk
+    return QUERY select tr.current_height <= tm.tank_warning_level
+                 from water_tank_readings as tr
+                          join water_tanks_models tm on tr.tank_model_fk = tm.pk
                  where tr.pk = reading_pk;
 end;
 $$
@@ -14,10 +14,10 @@ create function add_to_worklist2() returns trigger as
 $$
 begin
     if is_caution_level(new.pk) then
-        insert into manager_worklist (time_estimate_fk, resident_fk, tank_type)
-        SELECT 6, u.pk, tr.tank_type_fk
-        from tank_readings tr
-                 join "user" u on tr.tank_owner_fk = u.pk
+        insert into manager_worklist (time_estimate_fk, resident_fk, tank_type_fk)
+        SELECT 6, u.pk, 1
+        from water_tank_readings tr
+                 join residents u on tr.tank_owner_fk = u.pk
         where tr.pk = new.pk;
     end if;
     return new;
@@ -25,17 +25,21 @@ end;
 $$
     language plpgsql;
 
+
 create trigger add_to_worklist
     after insert
-    on tank_readings
+    on water_tank_readings
     for each row
 execute procedure public.add_to_worklist2();
 
-insert into tank_readings (current_height, tank_owner_fk, tank_type_fk, tank_model_fk)
-values (180, 1, 1, 1);
 
-insert into tank_readings (current_height, tank_owner_fk, tank_type_fk, tank_model_fk)
-values (50, 1, 1, 1);
-
-drop trigger add_to_worklist on tank_readings;
+drop trigger add_to_worklist on water_tank_readings;
 drop function add_to_worklist2();
+
+
+
+insert into water_tank_readings (current_height, tank_owner_fk, tank_model_fk)
+values (180, 1, 1);
+
+insert into water_tank_readings (current_height, tank_owner_fk, tank_model_fk)
+values (50, 1, 1);
